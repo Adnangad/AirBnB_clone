f1ab4b1b97c778c0ef7065a3c4eb480ba4d85933
Adnangad <gardobuyaadnan@gmail.com>
2024-02-10 07:28:45 -0800

    6-console

---
+++ console.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This is a cmd line interprator.
+"""
+import cmd
+from models import storage
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    this is usd to manipulate objects.
+    """
+    prompt = "(hbnb) "
+
+    def do_quit(self, args):
+        """
+        This cmd exits the prompt.
+        """
+        return True
+
+    def do_EOF(self, args):
+        """
+        This cmd exits the prompt.
+        """
+        return True
+
+    def do_emptyline(self, args):
+        None
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
