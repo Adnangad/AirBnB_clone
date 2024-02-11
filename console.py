b2116f4b3fd3d60b576e28c259397d7cf3388f93
Adnangad <gardobuyaadnan@gmail.com>
2024-02-11 00:40:11 -0800

    10-updated console to handle other classes

--- console.py
+++ console.py
@@ -7,6 +7,11 @@ from models.base_model import BaseModel
 import re
 from models import storage
 from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.state import State
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
